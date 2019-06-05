2125b30982ea1c46753ebffdbbcef432d7ec9e59
Basarat <basaratali@gmail.com>
2019-06-05 10:19:31 +1000

    expand vertical

--- src/gls.tsx
+++ src/gls.tsx
@@ -54,21 +54,33 @@ Grid.displayName = 'Grid';
 ///////////////////////////////////
 
 export interface VerticalProps extends React.HTMLProps<HTMLDivElement> {
-  spacing?: number
+  spacing?: number,
+
+  verticalAlign?: 'top' /** default */ | 'center' | 'bottom',
+  horizontalAlign?: 'left' /** default */ | 'right' | 'center',
 }
 
 /** 
  * Layout out children with a vertical margin between them
  */
 export const Vertical: React.SFC<VerticalProps> = (props) => {
-  const { className, ...otherProps } = props;
+  const {
+    className,
+    horizontalAlign,
+    verticalAlign,
+    ...otherProps
+  } = props;
 
   const klass = typestyle.classes(
     className,
     typestyle.style(
       csstips.content,
       csstips.vertical,
-      csstips.verticallySpaced(props.spacing == null ? Spacing.vertical : props.spacing)
+      csstips.verticallySpaced(props.spacing == null ? Spacing.vertical : props.spacing),
+      verticalAlign == 'center' && csstips.centerJustified,
+      verticalAlign == 'bottom' && csstips.endJustified,
+      horizontalAlign == 'right' && csstips.end,
+      horizontalAlign == 'center' && csstips.center,
     )
   );
 
