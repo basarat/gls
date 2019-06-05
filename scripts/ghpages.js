6932d16f7b5e2d615f1f5ab697f37ba8cacb9fd3
Basarat <basaratali@gmail.com>
2019-06-05 14:49:13 +1000

    cleanup

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -3,19 +3,19 @@ const path = require('path');
 const date = new Date();
 
 /** Directory */
-const directory = path.resolve(__dirname, '/../.docz/dist');
-console.log(__dirname, path.resolve(__dirname,'/..'));
+const directory = '.docz/dist';
+
+/** Repo */
+const repo = 'basarat/gls';
 
 /** Branch */
 const branch = 'gh-pages';
 
-/** Repo */
-const repo = 'https://' + process.env.GH_TOKEN + '@github.com/basarat/gls.git';
 
-ghpages.publish(directory, {
+ghpages.publish(path.resolve(__dirname, '..', directory), {
   message: `[ci skip] deployment (${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}-${date.getUTCHours()}-${date.getUTCMinutes()})`,
   branch: branch,
-  repo: repo,
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/' + repo + '.git',
   user: {
     name: 'basarat',
     email: 'basaratali@gmail.com'
