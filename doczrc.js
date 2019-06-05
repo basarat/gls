6e73e670f643b5470da85d61915895c5299fd344
Basarat <basaratali@gmail.com>
2019-06-05 12:44:35 +1000

    style fixes

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,12 @@
 export default {
-    title: 'General Layout System',
-    codeSandbox: true,
-    typescript: true,
+  title: 'General Layout System',
+  codeSandbox: true,
+  typescript: true,
+  themeConfig: {
+    styles: {
+      playground: `
+        padding: 32px
+      `
+    }
+  }
 }
