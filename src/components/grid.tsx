1fdac2a8564abb3c7c6b7ec1bc14ea7e1beed1f5
Basarat <basaratali@gmail.com>
2019-07-02 10:32:56 +1000

    GridUniform and GridAspect

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,30 +1,70 @@
 import * as typestyle from 'typestyle';
 import { types, classes } from 'typestyle';
 import * as React from 'react';
-import * as csstips from 'csstips';
-import { Spacing } from './common';
+import { Spacing, BoxUnit } from './common';
+import { boxUnitToString } from '../internal/utils';
 
-///////////////////////////////////
-// GRID 
-///////////////////////////////////
+/**
+ * Puts a (horizontal AND vertical) margin between each child
+ */
+export function gridSpaced(both: BoxUnit): string;
+export function gridSpaced(topAndBottom: BoxUnit, leftAndRight: BoxUnit): string;
+export function gridSpaced(topAndBottom: BoxUnit, leftAndRight = topAndBottom): string {
+  const vertical = boxUnitToString(topAndBottom);
+  const horizontal = boxUnitToString(leftAndRight);
+  return typestyle.style(
+    {
+      marginTop: '-' + vertical,
+      marginLeft: '-' + horizontal,
+      '&>*': {
+        marginTop: vertical,
+        marginLeft: horizontal,
+      }
+    } as types.CSSProperties
+  );
+};
 
-export interface GridProps extends React.HTMLProps<HTMLDivElement> {
-  spacing?: number
+export interface GridUniformProps extends React.HTMLProps<HTMLDivElement> {
+  spacing?: BoxUnit
 }
 
 /** 
  * Lays out children with a margin between them (horizontal and vertical)
  */
-export const Grid: React.FC<GridProps> = (props) => {
-  const { className, ...otherProps } = props;
+export const GridUniform: React.FC<GridUniformProps> = (props) => {
+  const { className, spacing = Spacing.grid.horizontal, ...otherProps } = props;
   const klass = classes(
     className,
-    typestyle.style(csstips.gridSpaced(
-      props.spacing == null ? Spacing.grid.vertical : props.spacing
-    )),
+    gridSpaced(spacing),
   );
   return (
-    <div {...otherProps} className={klass} data-comment='Grid' />
+    <div {...otherProps} className={klass} data-comment='GridUniform' />
   );
 }
-Grid.displayName = 'Grid';
+GridUniform.displayName = 'GridUniform';
+
+
+export interface GridAspectProps extends React.HTMLProps<HTMLDivElement> {
+  vertical?: BoxUnit
+  horizontal?: BoxUnit
+}
+
+/** 
+ * Lays out children with a margin between them (horizontal and vertical)
+ */
+export const GridAspect: React.FC<GridAspectProps> = (props) => {
+  const {
+    className,
+    vertical = Spacing.grid.vertical,
+    horizontal = Spacing.grid.horizontal,
+    ...otherProps
+  } = props;
+  const klass = classes(
+    className,
+    gridSpaced(vertical, horizontal),
+  );
+  return (
+    <div {...otherProps} className={klass} data-comment='GridAspect' />
+  );
+}
+GridAspect.displayName = 'GridAspect';
