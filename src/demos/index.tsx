cd7f9d848d8bfcff6d8994c3840a664df720dca0
Basarat <basaratali@gmail.com>
2019-07-18 03:28:17 +1000

    demos

--- src/demos/index.tsx
+++ src/demos/index.tsx
@@ -3,6 +3,27 @@
  */
 
 import React from "react";
+import * as typestyle from "typestyle";
 
 export const BlueDiv: React.FC = (props) => <div style={{ backgroundColor: "lightskyblue" }} children={props.children} />;
 export const PinkDiv: React.FC = (props) => <div style={{ backgroundColor: "lightpink" }} children={props.children} />;
+
+/** 
+ * Label 
+ */
+const labelClass = typestyle.style({
+});
+export const Label: React.FC = (props) => {
+  return <label className={labelClass} children={props.children} />;
+}
+
+/** 
+ *  Input
+ */
+const inputClass = typestyle.style({
+  width: 'calc(100% - 5px)' /** Docz weirdness */,
+  fontSize: '20px',
+});
+export const Input: React.FC = (_props) => {
+  return <input className={inputClass} />;
+}
