42b0b09276ed8a7145d97c99e5f1fbfce9a55fc2
Basarat <basaratali@gmail.com>
2019-07-18 15:19:28 +1000

    simplify scroll docs

--- src/styles/scroll.ts
+++ src/styles/scroll.ts
@@ -19,9 +19,7 @@ export const scrollVertical: types.NestedCSSProperties = {
 };
 
 /**
- * If you expect a child somewhere down in the tree to scroll
- * you need to tell the browser to prevent a scroll bar. 
- * Example use : `parent(scrollDisable) > child(scroll)`
+ * Cuts off the exccess content and disables the scroll bar
  */
 export const scrollDisable: types.NestedCSSProperties = {
   overflow: 'hidden'
