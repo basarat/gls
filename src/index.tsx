a92df6bd05010dca7bc38fe0501974fac6f9b2a2
Basarat <basaratali@gmail.com>
2019-07-18 14:17:50 +1000

    cleanup exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@
 export * from './common';
 
 /** Components */
-export * from './components/grid';
+export * from './components/containers';
 export * from './components/vertical';
 export * from './components/horizontal';
-export * from './components/containers';
+export * from './components/responsive';
+export * from './components/grid';
+export * from './components/spacers';
