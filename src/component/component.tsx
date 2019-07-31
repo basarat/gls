aca9fca2382ecf2e16d050086c104c25e2670446
Basarat <basaratali@gmail.com>
2019-07-31 16:36:42 +1000

    action padding

--- src/component/component.tsx
+++ src/component/component.tsx
@@ -1,5 +1,6 @@
 import { PaddingProp, ClassNameProp } from "../common";
 import * as typestyle from "typestyle";
+import { _processPadding } from "../internal/utils";
 
 /** 
  * A set of props that can help you build layout-customizable components
@@ -11,13 +12,24 @@ export interface ComponentProps extends
 
 /** 
  * Takes a set of component props 
- * @returns a css className that actions all the component props 
+ * - actions certain props to update the `props` className
+ * @returns `props` with actioned members omitted and className updated
  */
-export function component<T extends ComponentProps>(props: T, _defaults?: T): string {
+export function component<T extends ComponentProps>(props: T, _defaults?: T)
+  : Omit<T, 'padding'> {
   const styles: typestyle.types.NestedCSSProperties[] = [];
 
-  return typestyle.classes(
+  const {
+    padding,
+    ...result
+  } = props;
+
+  if (padding != null) styles.push(_processPadding(padding));
+
+  result.className = typestyle.classes(
     props.className,
     typestyle.style(...styles),
   );
+
+  return result;
 }
